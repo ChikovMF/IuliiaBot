9c767ad9e90cea5628415def48b0f424b4c3a678
ChikovMF <ChikovMF@yandex.ru>
2025-03-12 05:07:58 +0300

    Добавление контроллера для работы с транслитерацией

---
+++ src/Web.Api/Controllers/TransliterationController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Api.Controllers;
+
+/// <summary>
+/// Контроллер для работы с транслитерацией.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class TransliterationController : ControllerBase
+{
+    
+}
